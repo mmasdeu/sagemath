8f8856ab97cfcaa7b36234c6e8bc3db1adf95a00
Marc Masdeu <marc.masdeu@gmail.com>
2017-07-28 11:40:19 +0200

    Imported only the version function.

--- sagemath/check_version.py
+++ sagemath/check_version.py
@@ -5,11 +5,11 @@ from pkg_resources import parse_version
 # Here we test that the version is the correct one
 def check_version(version):
     try:
-        from sage.all import ZZ
+        from sage.all import version
     except ImportError:
         raise ValueError("Sage does not seem to be installed in this system. Please visit www.sagemath.org to fix this!")
     version = str(version)
-    installed_version = sage.all.version().replace(',','').split()[2]
+    installed_version = version().replace(',','').split()[2]
     if version.find('==') != -1:
         version = version.replace('==','')
         if parse_version(version) != parse_version(installed_version):
